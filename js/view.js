7eaddc0aa348f9bdb34f4523a62838ab66792fcc
GET Academy <terje@getacademy.no>
2024-10-20 15:17:26 +0200

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,20 +1,34 @@
 function updateView() {
     document.getElementById('app').innerHTML = /*HTML*/`
         <h1>Filer og mapper</h1>
+        Du er her: ${createSummarySelected()}<br/>
         ${createFoldersHtml()}
         ${createFilesHtml()}
         ${createEditFileHtml()}
     `;
 }
 
+function createSummarySelected() {
+    let id = model.app.currentId;
+    if (id == null) return '<i>rotmappe</i>';
+    let breadcrumbs = '';
+    while (id) {
+        let fileOrFolder = model.filesAndFolders.find(f => f.id == id);
+        if (breadcrumbs != '') breadcrumbs = ' > ' + breadcrumbs;
+        breadcrumbs = fileOrFolder.name + breadcrumbs;
+        id = fileOrFolder.parentId;
+    }
+    return breadcrumbs;
+}
+
 function createFoldersHtml() {
     let currentId = model.app.currentId;
     const currentFileOrFolder = model.filesAndFolders.find(f => f.id == currentId);
     let html = '';
-    if(currentFileOrFolder != null) {
+    if (currentFileOrFolder != null) {
         html = `📁 <a href="javascript:select(${currentFileOrFolder.parentId})">..</a><br/>`;
         if (currentFileOrFolder.hasOwnProperty('content')) currentId = currentFileOrFolder.parentId;
-    } 
+    }
     for (let folder of model.filesAndFolders) {
         if (folder.hasOwnProperty('content') || folder.parentId != currentId) continue;
         html += `📁 <a href="javascript:select(${folder.id})">${folder.name}</a><br/>`;
@@ -26,9 +40,9 @@ function createFoldersHtml() {
 function createFilesHtml() {
     let currentId = model.app.currentId;
     const currentFileOrFolder = model.filesAndFolders.find(f => f.id == currentId);
-    if(currentFileOrFolder != null) {
+    if (currentFileOrFolder != null) {
         if (currentFileOrFolder.hasOwnProperty('content')) currentId = currentFileOrFolder.parentId;
-    } 
+    }
     let html = '';
     for (let file of model.filesAndFolders) {
         if (!file.hasOwnProperty('content') || file.parentId != currentId) continue;
@@ -39,9 +53,9 @@ function createFilesHtml() {
 
 function createEditFileHtml() {
     const currentId = model.app.currentId;
-    if(currentId==null)return '';
+    if (currentId == null) return '';
     const currentFile = model.filesAndFolders.find(f => f.id == currentId);
-    if(!currentFile.hasOwnProperty('content')) return '';
+    if (!currentFile.hasOwnProperty('content')) return '';
     return /*HTML*/`
         <textarea>${currentFile.content}</textarea>    
         <br/>
